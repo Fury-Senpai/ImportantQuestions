4a70e68f413b703465abc721accee013a7baebd3
Fury-Senpai <143637458+Fury-Senpai@users.noreply.github.com>
2024-09-30 19:52:54 +0530

    Add files via upload

---
+++ StringPalindrome.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+string isPalindrome(string S)
+{
+    // Iterate over the range [0, N/2]
+    for (int i = 0; i < S.length() / 2; i++) {
+ 
+        // If S[i] is not equal to
+        // the S[N-i-1]
+        if (S[i] != S[S.length() - i - 1]) {
+            // Return No
+            return "No";
+        }
+    }
+    // Return "Yes"
+    return "Yes";
+}
+
+
+int main()
+{
+    string s;
+    cout<<"Enter a name\n";
+    cin>>s;
+    //string S = "ABCDCBA";
+    cout << isPalindrome(s);
+}
